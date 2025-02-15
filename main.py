42b6ded6159c9fd87d914cd247ef0e1976de1afe
your_github_usernamedsundrud <dsundrud@gmail.com>
2025-02-15 16:51:12 -0500

    screen setup

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import pygame
+from constants import *
 
 def main():
-	print("Starting asteroids!")
-
+    pygame.init()
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    while True:
+        for event in pygame.event.get():
+               if event.type == pygame.QUIT:
+                      return
+        screen.fill(pygame.Color(255,182,193))
+        pygame.display.flip()
+        
+		 # 3 seconds	
 if __name__ == "__main__":
     main()
